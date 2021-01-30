70c20b001a7cfd587f513a6f6bf25fe920554def
Descoberto <50559090+Descoberto@users.noreply.github.com>
2021-01-29 23:03:16 -0300

    Update GameOver.cs

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-// GameOver Class
+// GameOver
 public class GameOver : MonoBehaviour {
 
     // Command Update When Press Esc Key
