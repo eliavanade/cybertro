414ef93b840415605eb239ac975667c1b50dcc38
Descoberto <50559090+Descoberto@users.noreply.github.com>
2021-03-12 21:36:11 -0300

    Update gameManager.cs

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -72,11 +72,12 @@ public class gameManager : MonoBehaviour {
             pause = true;
         }
     } // Pause the game
-
+    
+    // Faz a verificação se as peças estão dentro ou fora da grade
     public bool dentroGrade(Vector2 posicao)
     {
         return ((int)posicao.x >= 0 && (int)posicao.x < largura && (int)posicao.y >= 0);
-    } // Faz a verificação se as peças estão dentro ou fora da grade
+    }
 
     public Vector2 arredonda(Vector2 nA)
     {
